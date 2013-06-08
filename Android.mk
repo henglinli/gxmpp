de3c827957f1fec96e10688f15ed9e2afbe08223
henglinli <henglinli@gmail.com>
2013-06-08 21:01:07 +0800

    update Android.mk

--- Android.mk
+++ Android.mk
@@ -15,7 +15,7 @@ expat_CFLAGS := \
 	-DHAVE_MEMMOVE
 
 expat_C_INCLUDES += \
-	$(LOCAL_PATH)/lib
+	$(LOCAL_PATH)/third_party/expat/lib
 
 # gxmpp
 # =====================================================
@@ -42,36 +42,42 @@ gxmpp_C_INCLUDES += \
 # expat static library
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(expat_SRC_FILES)
-LOCAL_CFLAGS += $(expat_CFLAGS)
+LOCAL_MODULE := expat_static
+
 LOCAL_C_INCLUDES += $(expat_C_INCLUDES)
 
-LOCAL_MODULE:= libexpat_static
+LOCAL_CFLAGS += $(expat_CFLAGS)
+
+LOCAL_SRC_FILES := $(expat_SRC_FILES)
 
 include $(BUILD_STATIC_LIBRARY)
 
 # gxmpp static library
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := $(gxmpp_SRC_FILES)
-LOCAL_CXXFLAGS += $(gxmpp_CXXFLAGS)
+LOCAL_MODULE := gxmpp_static
+
 LOCAL_C_INCLUDES += $(gxmpp_C_INCLUDES)
 
 #LOCAL_CFLAGS := -std=gnu++11
 
-LOCAL_MODULE:= libgxmpp_static
+LOCAL_CXXFLAGS += $(gxmpp_CXXFLAGS)
+
+LOCAL_SRC_FILES := $(gxmpp_SRC_FILES)
 
 include $(BUILD_STATIC_LIBRARY)
 
 # gxmpp shared library
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := $(wildcard examples/echo/*.cc) 
 
-LOCAL_CXXFLAGS += $(gxmpp_CXXFLAGS)
+LOCAL_MODULE := gxmpp
+
 LOCAL_C_INCLUDES += $(gxmpp_C_INCLUDES)
 
-LOCAL_STATIC_LIBRARIES := expat_static gxmpp_static 
+LOCAL_CXXFLAGS += $(gxmpp_CXXFLAGS)
 
-LOCAL_MODULE:= gxmpp
+LOCAL_SRC_FILES := $(wildcard examples/echo/*.cc) 
+
+LOCAL_STATIC_LIBRARIES := gxmpp_static expat_static  
 
 include $(BUILD_SHARED_LIBRARY)
